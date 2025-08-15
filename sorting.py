ab5fbdfef1f57a54dbaa9c70b21d16cd5aade559
Priyanka Thakare <39987742+priyankasT16@users.noreply.github.com>
2025-08-15 12:41:55 +0000

    added file to workspace

--- sorting.py
+++ sorting.py
@@ -18,6 +18,17 @@ def Bubble_sort(arr):
             break
     return arr
 
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(0,n):
+        key = arr[i]
+        j = i-1
+        while j>= 0 and key<arr[j+1]:
+            arr[j+1] = arr[j]
+            j-=1
+            arr[j+1] = key
+    return arr
+
 arr = [10,6,3,7]
 sorted_arr = Bubble_sort(arr)
 print(sorted_arr)
